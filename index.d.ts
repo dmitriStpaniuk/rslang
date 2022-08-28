70efd705868381e6f4664b5782974e5239468c5a
Dmitry.Stepaniuk <36957687+dmitriStpaniuk@users.noreply.github.com>
2022-08-28 13:53:49 +0300

    Revert "Vlad k branch"

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,6 @@ import { Theme as MuiTheme } from "@mui/material/styles";
 declare module '@emotion/react' {
   export interface Theme extends MuiTheme {}
   declare module '*.png';
-  declare module '*.mp3';
 }
 declare module '*.jpg';
 declare module '*.avif';
